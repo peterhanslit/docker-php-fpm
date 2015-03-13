856bf78d3a5490e9881ce3bad3c877eb16729267
Federico Ramallo <framallo@gmail.com>
2015-03-12 19:18:34 -0600

    showing nginx log

--- 50_init.sh
+++ 50_init.sh
@@ -3,5 +3,8 @@
 echo 'running php5-fpm'
 php5-fpm --nodaemonize --fpm-config /etc/php5/fpm/php-fpm.conf &
 
+echo 'showing nginx log'
+tail -f /var/log/nginx/error.log &
+
 echo 'running nginx'
 nginx
