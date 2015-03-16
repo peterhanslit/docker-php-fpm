528995971895bc18e850ca4b9f88056c09cc6edd
Federico Ramallo <framallo@gmail.com>
2015-03-16 11:46:20 -0600

    improved nginx error log output

--- 50_init.sh
+++ 50_init.sh
@@ -4,7 +4,9 @@ echo 'running php5-fpm'
 php5-fpm --nodaemonize --fpm-config /etc/php5/fpm/php-fpm.conf &
 
 echo 'showing nginx log'
-tail -f /var/log/nginx/error.log &
+mkdir -p /var/log/nginx/
+tail --follow=name --retry /var/log/nginx/error.log &
+
 
 echo 'running nginx'
 nginx
